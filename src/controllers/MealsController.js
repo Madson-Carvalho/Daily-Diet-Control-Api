230541c5fd3973259ce6f4299506fab44394a394
Mayra-kauane <mayrakauane64@gmail.com>
2024-03-27 11:00:22 -0300

    Criado os selects de consulta as refeições

--- src/controllers/MealsController.js
+++ src/controllers/MealsController.js
@@ -1,4 +1,4 @@
-const {prisma} = require("../utils");
+const { prisma } = require("../utils");
 
 class MealsController {
     async create(request, response) {
@@ -7,6 +7,7 @@ class MealsController {
 
             const meals = prisma.meals.create({
                 data: {
+                    name: requestBody.name,
                     description: requestBody.description,
                     dateTime: requestBody.dateTime,
                     isInDiet: requestBody.isInDiet,
@@ -30,6 +31,7 @@ class MealsController {
                     id: param.id
                 },
                 data: {
+                    name: requestBody.name,
                     description: requestBody.description,
                     dateTime: requestBody.dateTime,
                     isInDiet: requestBody.isInDiet
@@ -58,6 +60,94 @@ class MealsController {
         }
     }
 
+    async findByName(request, response) {
+        try {
+            const param = request.params;
+
+            const meal = await prisma.meals.findUnique({
+                where: {
+                    name: param.name,
+                },
+                select: {
+                    name: true,
+                    description: true,
+                    dateTime: true,
+                    isInDiet: true,
+                },
+            })
+
+            return response.json(meal);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
+
+    async findAll(response) {
+        try {
+            const meals = await prisma.meals.findMany()
+
+            return response.json(meals);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
+
+    async countAllMeals(response) {
+        try {
+            const quantityMeals = await prisma.meals.count()
+
+            return response.json(quantityMeals);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
+
+    async countAllMealsInDiet(response) {
+        try {
+            const quantityMealsInDiet = await prisma.meals.findMany({
+                include: {
+                    _count: {
+                        select: { isInDiet: true },
+                    },
+                },
+            })
+
+            return response.json(quantityMealsInDiet);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
+
+    async countAllMealsOutDiet(response) {
+        try {
+            const quantityMealsOutDiet = await prisma.meals.findMany({
+                include: {
+                    _count: {
+                        select: { isInDiet: false },
+                    },
+                },
+            })
+
+            return response.json(quantityMealsOutDiet);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
+
+    async getLongerMealSequence(response) {
+        try {
+            const longerMealSequence = await prisma.meals.groupBy({
+                by: ['dateTime'],
+                _sum: {
+                    isInDiet: true,
+                },
+            })
+
+            return response.json(longerMealSequence);
+        } catch (e) {
+            return response.response.status(409).send();
+        }
+    }
 }
 
 module.exports = MealsController;
