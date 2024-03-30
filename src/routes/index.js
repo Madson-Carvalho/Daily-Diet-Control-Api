4cb3dd0ea8a36b787ded037612886a97cd19ae07
RodrigoTDonde <rodrigotdonde@gmail.com>
2024-03-30 17:06:01 -0300

    Criei as rotas de busca, alteração e remoção para as refeições.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const {Router} = require("express");
 const usersRoutes = require('./users.routes')
+const mealsRoutes = require('./meals.routes')
 
 const routes = Router();
 
- routes.use('/api/v1/users', usersRoutes);
+ routes.use("/api/v1/users", usersRoutes);
+
+ routes.use("/api/v1/meals", mealsRoutes);
 
 module.exports = routes;
